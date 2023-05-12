65d0d259225cfcbe90237ad626ce4397c65d2b00
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-05-12 09:06:27 +0100

    novo projecto do jose

---
+++ ajuda-do-renato.js
@@ -0,0 +1,17 @@
+document.getElementById("button1").addEventListener("click",funcao1)
+document.getElementById("button2").addEventListener("click",funcao2)
+document.getElementById("button3").addEventListener("click",funcao3)
+
+
+
+function funcao1(){
+    document.getElementById("s").style.marginLeft="0"
+}
+function funcao2(){
+    document.getElementById("s").style.marginLeft="-33%"
+}
+   
+
+function funcao3(){
+    document.getElementById("s").style.marginLeft="-66%"
+}
