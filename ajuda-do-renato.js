84af05900a45f7605cd3b252da518eb3946bc132
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-05-13 19:35:31 +0100

    finalizando coisa que o jose nao sabe fazer

--- ajuda-do-renato.js
+++ ajuda-do-renato.js
@@ -11,18 +11,26 @@ let item3= document.getElementById("sss")
 
 
 function funçao1(){
+    
+    item2.style.animationName='none'
+    item3.style.animationName='none'
     s.style.marginLeft='0'
     item1.style.animationName='abrir'
-    item1.style.animationDelay='2s'
+    item1.style.animationDuration='2s'
 }
 function funçao2(){
+    item1.style.animationName='none'
+    
+    item3.style.animationName='none'
     s.style.marginLeft='-550px'
     item2.style.animationName='abrir'
-    item2.style.animationDelay='2s'
+    item2.style.animationDuration='2s'
 }
 function funçao3(){
-
+    item1.style.animationName='none'
+    item2.style.animationName='none'
+   
     s.style.marginLeft='-1080px'
     item3.style.animationName='abrir'
-    item3.style.animationDelay='2s'
+    item3.style.animationDuration='2s'
 }
