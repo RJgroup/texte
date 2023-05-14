1d738eac492ff17576fa2c4a85076f9a515758ef
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-05-14 12:11:53 +0100

    organizando o projecto finalizado

--- ajuda-do-renato.js
+++ ajuda-do-renato.js
@@ -1,17 +1,18 @@
 let button1= document.getElementById("button1")
 let button2= document.getElementById("button2")
 let button3= document.getElementById("button3")
+
 let s= document.getElementById('s')
+
 button1.addEventListener("click", funçao1)
 button2.addEventListener("click", funçao2)
 button3.addEventListener("click", funçao3)
+
 let item1= document.getElementById("s")
 let item2= document.getElementById("ss")
 let item3= document.getElementById("sss")
 
-
 function funçao1(){
-    
     item2.style.animationName='none'
     item3.style.animationName='none'
     s.style.marginLeft='0'
@@ -20,16 +21,15 @@ function funçao1(){
 }
 function funçao2(){
     item1.style.animationName='none'
-    
     item3.style.animationName='none'
     s.style.marginLeft='-550px'
     item2.style.animationName='abrir'
     item2.style.animationDuration='2s'
+    item2.style.animationDirection='left'
 }
 function funçao3(){
     item1.style.animationName='none'
     item2.style.animationName='none'
-   
     s.style.marginLeft='-1080px'
     item3.style.animationName='abrir'
     item3.style.animationDuration='2s'
