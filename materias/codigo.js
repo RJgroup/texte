ba99ebec2b5380a95a1974f3015fa75579ba88c3
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-06-20 10:04:33 +0100

    ...

---
+++ materias/codigo.js
@@ -0,0 +1,27 @@
+let pressao= String(Math.floor(Math.random()*(99-10)+10) )
+let temperatura=String(Math.floor(Math.random()*(99-10)+10) )
+let R=String(Math.floor(Math.random()*(99-10)+10))
+let moles= String(Math.floor(Math.random()*(99-10)+10))
+
+
+let area=document.getElementById("code")
+
+area.innerHTML=pressao+temperatura+R+moles
+
+pressao=Number(pressao)
+temperatura=Number(temperatura)
+R=Number(R)
+moles=Number(moles)
+
+let volume= (moles*R*temperatura)/pressao
+volume=Math.floor(volume)
+
+ function verificar() {
+    var input=document.getElementById("codigo")
+    if(input.value==volume){
+        document.getElementById('verific').style.display='none'
+        document.getElementById('res').style.display='block'
+    }else{
+        document.write('errado')
+    }
+ }
