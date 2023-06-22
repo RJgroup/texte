4de9c667cbfbeaddff937a38275c0ea462e73f06
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-06-22 08:29:50 +0100

    ....

--- materias/script-inicial.js
+++ materias/script-inicial.js
@@ -1,5 +1,6 @@
-var togle=document.getElementById('togle')
-togle.addEventListener('click',funcao)
-function funcao() {
-    togle.classList.toggle('renato')
+
+function funcao(a) {
+    
+    a.addEventListener('click',funcao)
+    a.classList.toggle('renato')
 }
