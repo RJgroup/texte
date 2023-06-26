e86143a10d812b78fbf3b743a62c3fbad9aba611
Ayguer_Acabane <132191421+Ayguer@users.noreply.github.com>
2023-06-26 14:28:52 +0100

    ....

--- materias/codigo.js
+++ materias/codigo.js
@@ -7,10 +7,10 @@ let moles= String(Math.floor(Math.random()*(99-10)+10))
 let area=document.getElementById("code")
 
 let jose=document.getElementById('linkjose')
-let simao=document.getElementById('linksimao')
+
 let renato= document.getElementById('linkrenato')
 jose.href=`https://wa.me/244933576607?text=Este é o meu codigo ${pressao+temperatura+R+moles}`
-simao.href=`https://wa.me/244957892985?text=Este é o meu codigo ${pressao+temperatura+R+moles}`
+
 renato.href=`https://wa.me/244944200401?text=Este é o meu codigo ${pressao+temperatura+R+moles}`
 
 area.innerHTML=pressao+temperatura+R+moles
